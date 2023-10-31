d02cc5082844f37527dd2bcb3e67d2a40fbf7c38
Cordelia Snape <snapecordelia@gmail.com>
2023-10-31 18:27:47 +0000

    end lab 7 progress

--- server.js
+++ server.js
@@ -6,5 +6,17 @@ const app = express();
 app.use(cors());
 
 // add your endpoints here
+const data = require("./data/weather.json");
+
+// console.log(data[0].lon);
+
+function findWeather(data) {
+  return data.filter(lat, lon, searchQuery);
+}
+
+app.get("/weather", (req, res) => {
+  const { lat, lon, searchQuery } = req.query;
+  console.log(findWeather(lat, loc));
+});
 
 app.listen(PORT, () => console.log(`App is running PORT ${PORT}`));
